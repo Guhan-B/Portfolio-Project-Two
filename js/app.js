9e8fa2e169718d61fec4610f13c3422bd63bfb32
Guhan <bkguhan2001@gmail.com>
2020-05-26 22:36:56 +0530

    Added GSAP load animation and minor fixes

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ $('.burger').click(function() {
     $('nav').toggleClass('show');
     $('body').toggleClass('overflow');
 });
+
+// GSAP Javascript
+
+window.onload = function() {
+    TweenMax.to('#dev-img', 2, { width: '38vw', opacity: 1, ease: Circ.easeOut });
+    TweenMax.to('.home', 2, { opacity: 1, ease: Power1.easeOut });
+    TweenMax.to(".footer-a", 0.75, { opacity: 1, margin: "0 2rem", delay: 2, ease: Circ.easeOut });
+    TweenMax.to(".line", 0.75, { scaleY: 1, delay: 2, ease: Circ.easeOut });
+}
